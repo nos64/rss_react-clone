ccda5829b76d76498512070e03c5aa3ea0abebaa
Mikhail Nosov <manosov1984@gmail.com>
2022-10-03 21:07:03 +0400

    refactor: please make in into render

--- first-app/src/components/CardList/CardList.tsx
+++ first-app/src/components/CardList/CardList.tsx
@@ -9,28 +9,29 @@ export default class CardList extends Component {
   constructor(props: ICard[]) {
     super(props);
   }
-  cards = (
-    <ul className={style.card__list}>
-      {cardDB.map((card) => (
-        <li key={card.id}>
-          <Card
-            id={''}
-            image={card.image}
-            brand={card.brand}
-            model={card.model}
-            year={card.year}
-            color={card.color}
-            colorID={''}
-            favorite={false}
-            doors={card.doors}
-            volume={card.volume}
-            owners={card.owners}
-          />
-        </li>
-      ))}
-    </ul>
-  );
   render() {
-    return <>{this.cards}</>;
+    return (
+      <>
+        <ul className={style.card__list}>
+          {cardDB.map((card) => (
+            <li key={card.id}>
+              <Card
+                id={''}
+                image={card.image}
+                brand={card.brand}
+                model={card.model}
+                year={card.year}
+                color={card.color}
+                colorID={''}
+                favorite={false}
+                doors={card.doors}
+                volume={card.volume}
+                owners={card.owners}
+              />
+            </li>
+          ))}
+        </ul>
+      </>
+    );
   }
 }
